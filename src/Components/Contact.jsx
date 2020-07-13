050a7ab81a723d7ac45acc4974fd157d12b98812
lukelin1991 <lukelin1991@yahoo.com>
2020-07-13 18:00:54 -0400

    created contact component

---
+++ src/Components/Contact.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div id="contact" className="section-wrapper"> 
+            <div className="contact-title">
+                <h3>Contact</h3>
+            </div>
+
+            <div className="email-wrapper">
+                <a href="mailto: lukelin1991@yahoo.com" target="_blank" rel="noopener noreferrer">lukelin1991@yahoo.com</a>
+            </div>
+        </div>
+    )
+}
