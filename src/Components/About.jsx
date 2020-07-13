e59ec9a655a5bf9ae7112d66b67b9f05951c5835
lukelin1991 <lukelin1991@yahoo.com>
2020-07-13 18:31:00 -0400

    making skills

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,3 +1,31 @@
-import React from 'react';
-import Luke from '../Images/_____'
+import React from "react";
+import Luke from "../Images/_____";
 
+const About = () => {
+  return (
+    <div>
+      <div>
+        <h3>About Me</h3>
+        <section id="about-luke">
+          <div>
+            <img />
+          </div>
+
+          <div>
+            <p>
+              Software Engineer | Full-Stack Web Developer | React Developer
+            </p>
+
+            <p>Hello world,</p>
+            <p>I'm Luke</p>
+            <p>I'm a full-stack web developer.</p>
+            <p>I live in New Jersey, USA.</p>
+            <p>I'd love to <a href="https://www.linkedin.com/in/luke-j-lin/" target="_blank" rel="noopener noreferrer">connect</a> with companies and people!</p>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default About;
