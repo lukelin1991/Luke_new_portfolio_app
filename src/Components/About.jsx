8f7b1c8983a5a24a66c2411b9ae0409b73ba1464
lukelin1991 <lukelin1991@yahoo.com>
2020-07-15 21:11:31 -0400

    adjustments

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -17,7 +17,7 @@ const About = () => {
             </p>
 
             <p>Hello world,</p>
-            <p>I'm Luke</p>
+            <p>I'm Luke, and I love music.</p>
             <p>I'm a full-stack web developer.</p>
             <p>I live in New Jersey, USA.</p>
             <p>I'd love to <a href="https://www.linkedin.com/in/luke-j-lin/" target="_blank" rel="noopener noreferrer">connect</a> with companies and people!</p>
