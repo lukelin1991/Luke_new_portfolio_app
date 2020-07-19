7169c6253a9e80ef576c2d81b5cdd522c0b15562
lukelin1991 <lukelin1991@yahoo.com>
2020-07-19 14:10:21 -0400

    worked resume

---
+++ src/Components/Resume.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Resume = () => {
+    return (
+        <div>
+            <div>
+                <div>
+                    <h3>Resume</h3>
+                </div>
+
+                <div className="resume-icon-wrapper">
+                    <a href="https://docs.google.com/document/d/1RcOo1DJxGRZzUnmE-aqA1j7qQ4u_LuYzeaRj3OMgvYo/edit?usp=sharing" target="_blank" rel="noopener noreferrer">
+                        <i className="fas fa-file-pdf" title="Luke Resume"></i>
+                    </a>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Resume
