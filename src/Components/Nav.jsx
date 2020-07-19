9ab4f6c9b62b3303c5ed6f94ce7538d1faa0ea6b
lukelin1991 <lukelin1991@yahoo.com>
2020-07-19 15:31:12 -0400

    nav added

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -14,8 +14,12 @@ const Nav = (props) => {
         </div>
       </ul>
 
-      <ul>
-          
+      <ul className="ul-nav">
+        <a href="/#about" className="nav-item"><li>About</li></a>
+        <a href="/#skills" className="nav-item"><li>Skills</li></a>
+        <a href="/#projects" className="nav-item"><li>Projects</li></a>
+        <a href="/#resume" className="nav-item"><li>Resume</li></a>
+        <a href="/#contact" className="nav-item"><li>Contact</li></a>
       </ul>
     </nav>
   );
